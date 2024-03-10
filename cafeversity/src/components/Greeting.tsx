3ed3f29cffcd2317320401fb3c29a3c06a2678d8
AleksanderShemarov <shemarov9822@gmail.com>
2024-03-10 20:45:39 +0300

    Info block creation from a data array

--- cafeversity/src/components/Greeting.tsx
+++ cafeversity/src/components/Greeting.tsx
@@ -5,32 +5,40 @@ import emptyImg from '../../public/no_image1.jpg';
 
 export default function Greeting() {
 
-    const listLines : string[] = [
-        "The first line: Lorem, ipsum dolor.",
-        "The second line: Lorem ipsum dolor sit amet.",
-        "The third line: Lorem ipsum dolor sit.",
+    const allData : [StaticImageData, string[]][] = [
+        [
+            emptyImg,
+            [
+                "The first line: Lorem, ipsum dolor.",
+                "The second line: Lorem ipsum dolor sit amet.",
+                "The third line: Lorem ipsum dolor sit.",
+            ],
+        ],
     ];
-
-    const imgFile : StaticImageData = emptyImg;// it is for images adding into several info blocks
 
     return (
         <div className={styles.features}>
-            <div className={styles.know0}>
-                <div className={styles.feature_picture}>
-                    <Image src={imgFile} alt="picture0" style={{
-                        width: "20vw",
-                        height: "250px",
-                        borderRadius: "24px",
-                    }}></Image>
-                </div>
-                <div className={styles.feature_list}>
-                    <ul id={styles.list}>
-                        {listLines.map((listLine : string, index : number) => 
-                            <li key={index}>{listLine}</li>
-                        )}
-                    </ul>
-                </div>
-            </div>
+            {
+                allData.map(
+                    (datum, index) => 
+                    <div className={styles.know0} key={index}>
+                        <div className={styles.feature_picture}>
+                            <Image src={datum[0]} alt="picture0" style={{
+                            width: "20vw",
+                            height: "250px",
+                            borderRadius: "24px",
+                            }}></Image>
+                        </div>
+                        <div className={styles.feature_list}>
+                            <ul id={styles.list}>
+                                {datum[1].map((datumLine : string, index : number) => 
+                                    <li key={index}>{datumLine}</li>
+                                )}
+                            </ul>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     )
 }
