9559d40256660a14134eca829002fc507fe38e3a
AleksanderShemarov <shemarov9822@gmail.com>
2024-03-15 00:41:34 +0300

    Full calibration of the "FoodList.tsx" file. Image component is input into separate div

--- cafeversity/src/components/FoodList.tsx
+++ cafeversity/src/components/FoodList.tsx
@@ -2,17 +2,11 @@
 
 import styles from "@/app/commonMenu/commonMenu.module.css";
 import Image from "next/image";
-import maczanka from "../../public/Мачанка_з_блінамі.jpeg";
 import ImageText from "./ImageText";
 import { useEffect, useState } from "react";
 
 
 export default function FoodList() {
-
-    const [title, setTitle] = useState<string>("Назва Ежы");
-    const [ing, setIng] = useState<string>("Склад: прадукт №1 (_%), прадукт №2 (_%), прадукт №3 (_%).");
-    const [portion, setPortion] = useState<number>(200);
-    const [price, setPrice] = useState<number>(0.00);
 
     interface Data {
         id: number,
@@ -45,7 +39,7 @@ export default function FoodList() {
             food_portion: 2000,
             cost: 1.23,
             imagePath: "/no_image1.jpg",
-        }// imagePath: "/image_folder/no_image1.jpg", (This example is correct too)
+        }// "/no_image1.jpg" – An each picture path must be started with "/" and a picture's file (for example, "no_image1.jpg") after it 
     ]);// You will see these data a few moments until the dishes_BY data fetching will be ended. If you see these data all time, data fetching is failed.
 
     useEffect(() => {
@@ -65,44 +59,53 @@ export default function FoodList() {
 
     return (
         <div className={styles.list_fields_zone}>
-            <div id={styles.list_field}>
-                <Image src={maczanka} alt="Мачанка_з_блінамі" style={{
-                    width: "20vw",
-                    height: "250px",
-                    border: "7px solid gold",
-                    borderRadius: "30px",
-                    margin: "0 auto",
-                    marginTop: "10px",
-                    marginBottom: "10px",
-                }}></Image>
-                <ImageText
-                    title={title}
-                    ingredients={ing}
-                    portion={portion}
-                    cost={price}
-                />
-            </div>
-            <div>{products.map((product) => 
-            <div key={product.id}>
-                <p>Product&apos;s Name: {product.food_name}</p>
-                <p>Product&apos;s Composition: {product.includes}</p>
-                <p>Is it spice? -&gt; {Boolean(product.spicy).toString().charAt(0).toUpperCase() + Boolean(product.spicy).toString().slice(1)}.<br />
-                Is it for vegetarians? -&gt; {Boolean(product.vegetarian).toString().charAt(0).toUpperCase() + Boolean(product.vegetarian).toString().slice(1)}.
-                <br />Is it for vegans? -&gt; {Boolean(product.vegan).toString().charAt(0).toUpperCase() + Boolean(product.vegan).toString().slice(1)}.</p>
-                <p>Protein (g): {product.protein}. Fats (g): {product.fats}. Carbohydrates (g): {product.carbohydrates}</p>
-                <p>Amino Acids & Others: {product.amino_acids}</p>
-                <p>Portion (g): {product.food_portion}. Price: {product.cost}</p>
-                <p>&quot;{product.imagePath}&quot; It is incorrect image path, because it starts with &quot;./&quot;,<br />
-                but it must be started with &quot;/&quot;</p>
-                <p>{product.imagePath[0] === "." ? product.imagePath.slice(1) : product.imagePath}</p>
-                <p>{product.imagePath.slice(product.imagePath.lastIndexOf("/")+1)}</p>
-                <Image
-                    src={product.imagePath.slice(product.imagePath.lastIndexOf("/"))}
-                    alt={product.imagePath.slice(product.imagePath.lastIndexOf("/")+1)}
-                    width={200}
-                    height={150}
-                ></Image>
-            </div>)}</div>
+            {products.map((product) => 
+                <div id={styles.list_field} key={product.id}>
+                    <div id={styles.list_field_image}>
+                        <Image
+                            src={product.imagePath.slice(product.imagePath.lastIndexOf("/"))}
+                            alt={product.imagePath.slice(product.imagePath.lastIndexOf("/"))+1}
+                            width={318}
+                            height={216}
+                            style={{
+                                border: "7px solid gold",
+                                borderRadius: "30px",
+                                margin: "auto",
+                                marginTop: "10px",
+                                marginBottom: "10px",
+                            }}
+                        ></Image>
+                    </div>
+                    <ImageText
+                        title={product.food_name}
+                        ingredients={product.includes}
+                        portion={product.food_portion}
+                        cost={product.cost}
+                    />
+                </div>
+            )}
         </div>
     )
 }
+
+
+/* <div key={product.id}>
+    <p>Product&apos;s Name: {product.food_name}</p>
+    <p>Product&apos;s Composition: {product.includes}</p>
+    <p>Is it spice? -&gt; {Boolean(product.spicy).toString().charAt(0).toUpperCase() + Boolean(product.spicy).toString().slice(1)}.<br />
+    Is it for vegetarians? -&gt; {Boolean(product.vegetarian).toString().charAt(0).toUpperCase() + Boolean(product.vegetarian).toString().slice(1)}.
+    <br />Is it for vegans? -&gt; {Boolean(product.vegan).toString().charAt(0).toUpperCase() + Boolean(product.vegan).toString().slice(1)}.</p>
+    <p>Protein (g): {product.protein}. Fats (g): {product.fats}. Carbohydrates (g): {product.carbohydrates}</p>
+    <p>Amino Acids & Others: {product.amino_acids}</p>
+    <p>Portion (g): {product.food_portion}. Price: {product.cost}</p>
+    <p>&quot;{product.imagePath}&quot; It is incorrect image path, because it starts with &quot;./&quot;,<br />
+    but it must be started with &quot;/&quot;</p>
+    <p>{product.imagePath[0] === "." ? product.imagePath.slice(1) : product.imagePath}</p>
+    <p>{product.imagePath.slice(product.imagePath.lastIndexOf("/")+1)}</p>
+    <Image
+        src={product.imagePath[0] === "." ? product.imagePath.slice(product.imagePath.lastIndexOf("/")) : product.imagePath}
+        alt={product.imagePath.slice(product.imagePath.lastIndexOf("/")+1)}
+        width={200}
+        height={150}
+    ></Image>
+</div> */
--- cafeversity/src/components/ImageText.tsx
+++ cafeversity/src/components/ImageText.tsx
@@ -12,10 +12,10 @@ export default function ImageText({ title, ingredients, portion, cost } : ImageD
     return (
         <div className={styles.text_lines}>
             <p id={styles.food_title}>{title}</p>
-            <p id={styles.food_ing}>{ingredients}</p>
+            <p id={styles.food_ing}>Склад: {ingredients}</p>
             <div className={styles.portia_price}>
-                <p id={styles.portia}>{portion} граммаў</p>
-                <p id={styles.price}>{cost} BYN</p>
+                <p id={styles.portia}>Вага порцыі: {portion} граммаў</p>
+                <p id={styles.price}>Кошт: {cost} BYN</p>
             </div>
         </div>
     )
