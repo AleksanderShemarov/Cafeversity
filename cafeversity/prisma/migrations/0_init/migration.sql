d751417a4fee1b1ef57e3774c6955a3805f36e93
AleksanderShemarov <shemarov9822@gmail.com>
2024-03-27 15:50:56 +0300

    Making the Prisma baselining for existing SQLite3 database

---
+++ cafeversity/prisma/migrations/0_init/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "dishes_BY" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "food_name" TEXT NOT NULL,
+    "includes" TEXT NOT NULL,
+    "spicy" BOOLEAN NOT NULL,
+    "vegetarian" BOOLEAN NOT NULL,
+    "vegan" BOOLEAN NOT NULL,
+    "protein" DECIMAL DEFAULT 0.00,
+    "fats" DECIMAL DEFAULT 0.00,
+    "carbohydrates" DECIMAL DEFAULT 0.00,
+    "amino_acids" TEXT DEFAULT '????????, ?????????, ??????',
+    "food_portion" DECIMAL DEFAULT 0.00,
+    "cost" DECIMAL DEFAULT 0.00,
+    "imagePath" TEXT NOT NULL
+);
+
