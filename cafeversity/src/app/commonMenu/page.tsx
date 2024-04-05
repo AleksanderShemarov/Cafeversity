345e779ff27f54a672656273f5d47de618791f25
AleksanderShemarov <shemarov9822@gmail.com>
2024-04-05 15:58:57 +0300

    Calculator Component is added on Common Menu page. Small style settings

--- cafeversity/src/app/commonMenu/page.tsx
+++ cafeversity/src/app/commonMenu/page.tsx
@@ -5,6 +5,7 @@ import styles from "./commonMenu.module.css";
 import CommonLayout from "@/components/CommonLayout";
 import FoodList from "@/components/FoodList";
 import { useState } from "react";
+import Calculator from "@/components/Calculator";
 
 
 export default function CommonMenuPage() {
@@ -16,19 +17,22 @@ export default function CommonMenuPage() {
             <div id={styles.main_part}>
                 <h1>Просты спіс ежы на сённяшні дзень.</h1>
                 <div id={styles.calculator_div} style={{
-                    right: click ? "0px" : "-550px",
+                    right: click ? "0px" : "-500px",
                 }}>
-                    <p id={styles.temporarily_p}>Calculator Field</p>
+                    {/* <p id={styles.temporarily_p}>Calculator Field</p> */}
                     <button
                         type="button"
                         style={{
                             writingMode: "vertical-lr",
                             textOrientation: "upright",
+                            marginLeft: "15px",
+                            marginRight: "5px",
                         }}
                         onClick={() => setClick(!click)}
                     >
-                        {click ? "Pushed" : "Push"}
+                        {click ? "Close Calculator" : "Open Calculator"}
                     </button>
+                    <Calculator />
                 </div>
                 <FoodList />
             </div>
