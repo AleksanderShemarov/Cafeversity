86c52edd125e3df914ffa9d1812ad0bd1c0ff346
AleksanderShemarov <shemarov9822@gmail.com>
2024-04-07 23:10:52 +0300

    Calculator Component was in RecipeDay page temporarily. Now it is removed.

--- cafeversity/src/app/news/recipeday/page.tsx
+++ cafeversity/src/app/news/recipeday/page.tsx
@@ -1,12 +1,10 @@
 import styles from "@/app/news/news.module.css";
-import Calculator from "@/components/Calculator"; // temporarily added for the "recipeday" page
 
 
 export default function RecipeDay () {
     return (
         <div id={styles.main_part}>
             <h1>Сённяшнія Рэцэпты</h1>
-            <Calculator />
         </div>
     )
 }
