4d30281796cf01c9cd641ecf7487953dd88da163
AleksanderShemarov <shemarov9822@gmail.com>
2024-04-21 17:18:26 +0300

    Preparing the ArticleBar Component for a multiple using by data from SQLite3 database and Prisma ORM

--- cafeversity/src/app/news/foodpeople/page.tsx
+++ cafeversity/src/app/news/foodpeople/page.tsx
@@ -1,14 +1,50 @@
 import styles from "@/app/news/foodpeople/foodpeople.module.css";
 import SearchLine from "@/components/SearchLine";
 import ArticleBar from "@/components/ArticleBar";
+import Gordon_Ramsay from "../../../../public/Gordon_Ramsay_and_the_dish.jpg";
+import Gordon_Ramsay_cooking from "../../../../public/Gordon_Ramsay_cooking.jpg";
+import noImage from "../../../../public/no_image1.jpg";
 
 
 export default function FoodPeople () {
+
+    const articlesBars = [
+        {
+            id: 1,
+            image: Gordon_Ramsay,
+            imageName: "Gordon_Ramsay_and_the_dish.jpg",
+            articleHead: "Гордан Рамсей",
+            articleShort: "Легенда кулінарнага мастацтва, рэстаратар ды тэле-зорка. 7 зорак Мішлэну і шэф-повар, якога ведаюць ва ўсім свеце.",
+        },
+        {
+            id: 2,
+            image: Gordon_Ramsay_cooking,
+            imageName: "Gordon_Ramsay_cooking.jpg",
+            articleHead: "Плануем...",
+            articleShort: "У хуткім часе будуць даданыя новыя асобы. Мы працуем над гэтым.",
+        },
+        {
+            id: 3,
+            image: noImage,
+            imageName: "no_image1.jpg",
+            articleHead: "Article Header",
+            articleShort: "Short Description.",
+        },
+    ];
+
     return (
         <div id={styles.main_part}>
             <h1>Знакамітыя людзі аб Ежы</h1>
             <SearchLine />
-            <ArticleBar />
+            {articlesBars.map((articleBar) => 
+                <ArticleBar
+                    key={articleBar.id}
+                    picture={articleBar.image}
+                    pictName={articleBar.imageName}
+                    articleName={articleBar.articleHead}
+                    shortText={articleBar.articleShort}
+                />
+            )}
         </div>
     )
 }
--- cafeversity/src/components/ArticleBar.tsx
+++ cafeversity/src/components/ArticleBar.tsx
@@ -1,18 +1,26 @@
 import styles from "@/app/news/foodpeople/foodpeople.module.css";
-import Gordon_Ramsay from "../../public/Gordon_Ramsay_and_the_dish.jpg";
-import Image from "next/image";
+// import Gordon_Ramsay from "../../public/Gordon_Ramsay_and_the_dish.jpg";
+import Image, { StaticImageData } from "next/image";
 
 
-export default function ArticleBar() {
+interface ArticleBarValues {
+    picture: StaticImageData,
+    pictName: string,
+    articleName: string,
+    shortText: string,
+}
+
+export default function ArticleBar({ picture, pictName, articleName, shortText } : ArticleBarValues) {
     return (
         <>
         {/* <div id={styles.article_bar_zone}> */}
             <div className={styles.article_bar_body}>
                 <div className={styles.article_bar_image}>
                     <Image
-                        src={Gordon_Ramsay}
-                        alt="Gordon_Ramsay_and_the_dish.jpg"
+                        src={picture}
+                        alt={pictName}
                         height={375}
+                        width={665}// Вырашыць праблему з габарытамі рознапамерных малюнкаў
                         style={{
                             borderTopLeftRadius: "27px",
                             borderBottomLeftRadius: "27px",
@@ -22,10 +30,8 @@ export default function ArticleBar() {
                 <div className={styles.article_bar_text}>
                     <div className={styles.article_text}>
                         {/* <h2 className={styles.article_name}>Article Header</h2> */}
-                        <h2 className={styles.article_name}>Гордан Рамсей</h2>
-                        <p className={styles.article_short}>
-                            Легенда кулінарнага мастацтва, рэстаратар ды тэле-зорка. 7 зорак Мішлэну і шэф-повар, якога ведаюць ва ўсім свеце.
-                        </p>
+                        <h2 className={styles.article_name}>{articleName}</h2>
+                        <p className={styles.article_short}>{shortText}</p>
                         {/* <p className={styles.article_short}>
                             Short Description: Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, recusandae.
                         </p> */}
