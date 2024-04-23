cdea49d6831795b26a783a1aacd749e4bdfb85a6
AleksanderShemarov <shemarov9822@gmail.com>
2024-04-23 18:02:08 +0300

    Making a template for an article page: the enter view is ready. To see it, write in the url line: "/news/foodpeople/1" or "/news/foodpeople/Gordon_Ramsay". First steps for using "pages/news/[ArticlePage2].tsx" like route path alternative instead of nested folders in the "app" directory

--- cafeversity/src/app/news/foodpeople/[foodpeople]/page.tsx
+++ cafeversity/src/app/news/foodpeople/[foodpeople]/page.tsx
@@ -1,10 +1,30 @@
 import styles from "@/app/news/foodpeople/[foodpeople]/article.module.css";
+import GordonRamsay_mainView from "@/../../public/Gordon_Ramsay_mainview.jpg";
+import Image from "next/image";
 
 
 export default function ArticlePage() {
     return (
         <div id={styles.main_part}>
             <h1>Тут будзе адабраны для прагляду артыкул.</h1>
+            <div id={styles.article_body}>
+                <div id={styles.article_enter_image}>
+                    <Image
+                        src={GordonRamsay_mainView}
+                        alt="Gordon_Ramsay_mainview.jpg"
+                        height={500}
+                        width={925}
+                        style={{
+                            borderTopLeftRadius: "27px",
+                            borderTopRightRadius: "27px",
+                        }}
+                    ></Image>
+                    <div id={styles.article_enter_name}>
+                        <h1 id={styles.article_enter_head}>Гордан Рамсей</h1>
+                        <p id={styles.article_enter_shorty}>Незвычайны лёс цікавага чалавека</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
