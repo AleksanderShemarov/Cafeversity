e85b9680640c8d49142d5fd964d156850fc01af9
AleksanderShemarov <shemarov9822@gmail.com>
2024-10-11 17:05:41 +0300

    My the biggest mistake in RangeInput2Handlers component was using 'defaultValue' attribute of input type="range" tag. Any value isn't checkable for React in it. Having changed 'defaultValue' on 'value' (it is checked by React and is changed by a component state), input type="range" tag Has statrted working correctly. Many thanks to Ben Honeywill, Front-End Software Engineer (the United Kingdom).

--- cafeversity/src/components/TastesSettings/CaloriesRange/RangeOfCalories.tsx
+++ cafeversity/src/components/TastesSettings/CaloriesRange/RangeOfCalories.tsx
@@ -7,26 +7,29 @@ const RangeInput2Handlers = ({ twohandRangeName = "" }) => {
 
     const lowerBorder: number = 1500;
     const upperBorder: number = 3000;
+    const step: number = 50;
 
     const [lowerRange, setLowerRange] = useState<number>(2300);
     const [upperRange, setUpperRange] = useState<number>(2500);        
 
     function lowerRangeCheck(event: React.ChangeEvent<HTMLInputElement>) {
+        event.preventDefault();
         const lowerRangeNum = Number(event.target.value);
 
-        // The simplest type is, but thumbs don't stop when equal
         if (lowerRangeNum <= upperRange) {
             setLowerRange(lowerRangeNum);
         }
+        // setLowerRange(Math.min(lowerRangeNum, upperRange)); // This is the same code as the condition above
     }
 
     function upperRangeCheck(event: React.ChangeEvent<HTMLInputElement>) {
+        event.preventDefault();
         const upperRangeNum = Number(event.target.value);
 
-        // The simplest type is, but thumbs don't stop when equal
         if (upperRangeNum >= lowerRange) {
             setUpperRange(upperRangeNum);
         }
+        // setUpperRange(Math.max(upperRangeNum, lowerRange)); // This is the same code as the condition above
     }
 
     return (
@@ -37,13 +40,21 @@ const RangeInput2Handlers = ({ twohandRangeName = "" }) => {
                 <span id={twohandRangeStyle.upRange}>{upperRange}</span>
             </div>
             <div className={twohandRangeStyle.twohandRangeTrack}></div>
+            {/* 
+                The main cause why conditions didn't have any effects on <input type="range"> tags 
+                was using 'defaultValue' attribute instead of 'value'.
+                React doesn't control the element values while using 'defaultValue' attribute.
+                React checks the element values and updates them by a component state using 'value' attribute.
+
+                These correct codelines I have found in Ben Honeywill's article "Building a Range Slider Component in React".
+            */}
             <input type="range" id="downRangePart" name={twohandRangeName}
-                min={lowerBorder} max={upperBorder} step="50"
-                defaultValue={lowerRange} onChange={lowerRangeCheck}
+                min={lowerBorder} max={upperBorder} step={step}
+                value={lowerRange} onChange={lowerRangeCheck}
             />
             <input type="range" id="upRangePart" name={twohandRangeName}
-                min={lowerBorder} max={upperBorder} step="50"
-                defaultValue={upperRange} onChange={upperRangeCheck}
+                min={lowerBorder} max={upperBorder} step={step}
+                value={upperRange} onChange={upperRangeCheck}
             />
         </div>
     )
