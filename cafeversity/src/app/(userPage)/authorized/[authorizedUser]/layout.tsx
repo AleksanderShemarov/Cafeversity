3db4cd695c30715e1a2b0145fe3f22fa13aa402d
AleksanderShemarov <shemarov9822@gmail.com>
2025-08-11 16:08:40 +0300

    Setups for BottomMenu and its buttons are: 1) height of BottomMenu is decreased. 2) "Menu" button has got its own path (the backlink from "../menu/page.tsx" is removed).

--- cafeversity/src/app/(userPage)/authorized/[authorizedUser]/layout.tsx
+++ cafeversity/src/app/(userPage)/authorized/[authorizedUser]/layout.tsx
@@ -27,21 +27,45 @@ export default function Layout ({ children, modal }: Readonly<{ children: React.
 
     if (pathname?.includes("/settingsPage")) {
         BottomBtns = [
-            { name: t("vanishingNavbar.menu"), icon: "/menu_list_icon.webp", icon_alt: "Menu_List_Icon", topMargin: 50, },
-            { name: t("vanishingNavbar.main"), icon: "/account_icon.png", icon_alt: "Account_Icon",
-                path: `${pathname.substring(0, pathname.lastIndexOf("/"))}` },
-            { name: t("vanishingNavbar.news"), icon: "/earth_planet.webp", icon_alt: "Earth_Icon", topMargin: 50, },
+            {
+                name: t("vanishingNavbar.menu"), icon: "/menu_list_icon.webp", icon_alt: "Menu_List_Icon", topMargin: 50,
+                path: `${pathname.substring(0, pathname.lastIndexOf("/"))}/menu`
+            },
+            {
+                name: t("vanishingNavbar.main"), icon: "/account_icon.png", icon_alt: "Account_Icon",
+                path: `${pathname.substring(0, pathname.lastIndexOf("/"))}`
+            },
+            {
+                name: t("vanishingNavbar.news"), icon: "/earth_planet.webp", icon_alt: "Earth_Icon", topMargin: 50,
+            },
+        ];
+    } else if (pathname?.includes("/menu")) {
+        const pathParts = pathname.split("/");
+        BottomBtns = [
+            {
+                name: t("vanishingNavbar.settings"), icon: "/settings-gear.png", icon_alt: "Settings_Icon", topMargin: 50,
+                path: `/${pathParts[1]}/${pathParts[2]}/settingsPage`
+            },
+            {
+                name: t("vanishingNavbar.main"), icon: "/account_icon.png", icon_alt: "Account_Icon",
+                path: `/${pathParts[1]}/${pathParts[2]}`
+            },
+            {
+                name: t("vanishingNavbar.news"), icon: "/earth_planet.webp", icon_alt: "Earth_Icon", topMargin: 50,
+            },
         ];
     } else {
         BottomBtns = [
             {
-                name: t("vanishingNavbar.settings"), icon: "/settings-gear.png", icon_alt: "Settings_Icon",
-                topMargin: 50, path: `${pathname}/settingsPage`
+                name: t("vanishingNavbar.settings"), icon: "/settings-gear.png", icon_alt: "Settings_Icon", topMargin: 50,
+                path: `${pathname}/settingsPage`
             },
-            { name: t("vanishingNavbar.menu"), icon: "/menu_list_icon.webp", icon_alt: "Menu_List_Icon", path: `${pathname}/menu` },
             {
-                name: t("vanishingNavbar.news"), icon: "/earth_planet.webp", icon_alt: "Earth_Icon",
-                topMargin: 50,
+                name: t("vanishingNavbar.menu"), icon: "/menu_list_icon.webp", icon_alt: "Menu_List_Icon",
+                path: `${pathname}/menu`
+            },
+            {
+                name: t("vanishingNavbar.news"), icon: "/earth_planet.webp", icon_alt: "Earth_Icon", topMargin: 50,
             },
         ];  
     }
--- cafeversity/src/app/(userPage)/authorized/[authorizedUser]/menu/page.tsx
+++ cafeversity/src/app/(userPage)/authorized/[authorizedUser]/menu/page.tsx
@@ -1,19 +1,7 @@
-"use client";
-
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-
-
 export default function MenuPage() {
-
-    const pathname = usePathname();
-
     return (
-        <>
-            <p style={{ fontSize: "2rem", fontWeight: "700" }}>Menu page for authorized users.</p>
-            <Link href={pathname.substring(0, pathname.lastIndexOf("/"))} style={{ fontSize: "1.8rem" }}>
-                To the general page
-            </Link>
-        </>
+        <p style={{ fontSize: "2rem", fontWeight: "700" }}>
+            Menu page for authorized users.
+        </p>
     );
 }
